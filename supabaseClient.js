1579fd7e94417a598b9912690aeff158e5795f05
Amornthip66 <oppok8803@gmail.com>
2026-08-31 23:08:32 +0700

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://supabase.co'
-const supabaseKey = 'your-anon-key'
+const supabaseUrl = 'https://slnakwquneyczqhxyfqb.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNsbmFrd3F1bmV5Y3pxaHh5ZnFiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcyMDc5MzMsImV4cCI6MjEwMjc4MzkzM30.ZvWe9iKb0ewo7viHNlo5Z8ecJVTSfxrHkz7qC35dnhw'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)

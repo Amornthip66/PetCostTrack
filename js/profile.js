39b22a3084c7d1fa6bc696a3424c4a14f8e2038f
PXAI <175822376+IMSMBPXAI@users.noreply.github.com>
2026-09-01 21:50:02 +0700

    fix pet04

--- js/profile.js
+++ js/profile.js
@@ -106,6 +106,7 @@ var Profile = (function() {
     function changePassword() {
         var session = Auth.getSession();
         if (!session || !session.access_token) return;
+        var email = Auth.getUser().email;
         var user = Auth.getUser();
         var email = user ? user.email : ((session && session.user) ? session.user.email : '');
         if (!email) {
